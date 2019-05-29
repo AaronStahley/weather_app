ff5f22d86957885bec8720c3df3f3d8a99fa8d36
AaronStahley <acstahle@asu.edu>
2019-05-28 18:36:42 -0700

    added initial colopsable tabs for forcasted data.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import store from "./store";
 import NavBar from "./components/NavBar";
 import CurrentWeather from "./components/CurrentWeather";
 import ForecastedWeather from "./components/ForecastedWeather";
+import Switch from "@material-ui/core/Switch";
 
 function App() {
   return (
     <Provider store={store}>
+      <Switch
+        // onChange={this.handleHiddenChange}
+        value="hidden"
+        color="primary"
+      />
       <div className="App">
         <header className="App-header">
           <NavBar />
--- src/components/ForecastedWeather.js
+++ src/components/ForecastedWeather.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { fetchForecastedWeather } from "../actions/weatherActions";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Typography from "@material-ui/core/Typography";
+// import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 class ForecastedWeather extends Component {
   componentWillMount() {
@@ -52,11 +57,34 @@ class ForecastedWeather extends Component {
 
     const daysData = daysArray.map(data => (
       <div key={data.dt_txt}>
-        <hr />
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            // expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className="name">
+              {this.formatDate(data.dt_txt)}
+              &nbsp; &nbsp; &nbsp; &nbsp;
+              {data.main.temp}º
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography>
+              Temp: {data.main.temp}º<br />
+              Conditions: {data.weather[0].main}
+              <br />
+              Conditions Desc: {data.weather[0].description}
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <br />
+
+        {/* <hr />
         <h4>Date: {this.formatDate(data.dt_txt)}</h4>
         <p>Temp: {data.main.temp}º</p>
         <p>Conditions: {data.weather[0].main}</p>
-        <p>Conditions Desc: {data.weather[0].description}</p>
+        <p>Conditions Desc: {data.weather[0].description}</p> */}
       </div>
     ));
 
